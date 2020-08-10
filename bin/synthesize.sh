0bf8301c75888e505a62eedd125786ee0d38b1ad
Joshua Bassett <hello@joshbassett.info>
2020-08-10 11:38:44 +1000

    Don't program the DE10-Nano if synthesizing fails

--- bin/synthesize.sh
+++ bin/synthesize.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 cd quartus
-quartus_sh --flow compile tecmo
-quartus_pgm -m jtag -c 1 -o "p;output_files/tecmo.sof@2"
+quartus_sh --flow compile tecmo && quartus_pgm -m jtag -c 1 -o "p;output_files/tecmo.sof@2"
